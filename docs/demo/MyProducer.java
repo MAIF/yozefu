dd5a1174dee24c4804090dcc6b6b42202e72d798
Yann Prono <yann.prono@telecomnancy.net>
2025-09-22 09:27:20 +0200

    build: update java deps

--- docs/demo/MyProducer.java
+++ docs/demo/MyProducer.java
@@ -1,15 +1,15 @@
-//JAVA 21+
+//JAVA 25+
 //REPOS central,confluent=https://packages.confluent.io/maven
-//DEPS com.fasterxml.jackson.core:jackson-databind:2.19.2
-//DEPS com.fasterxml.jackson.dataformat:jackson-dataformat-xml:2.19.2
-//DEPS org.apache.kafka:kafka-clients:4.0.0
+//DEPS com.fasterxml.jackson.core:jackson-databind:2.20.0
+//DEPS com.fasterxml.jackson.dataformat:jackson-dataformat-xml:2.20.0
+//DEPS org.apache.kafka:kafka-clients:4.1.0
 //DEPS io.confluent:kafka-protobuf-serializer:8.0.0
 //DEPS io.confluent:kafka-avro-serializer:8.0.0
 //DEPS io.confluent:kafka-json-schema-serializer:8.0.0
 //DEPS io.confluent:kafka-protobuf-serializer:8.0.0
-//DEPS org.slf4j:slf4j-nop:2.0.17
+//DEPS org.slf4j:slf4j-nop:2.0.16
 //DEPS tech.allegro.schema.json2avro:converter:0.3.0
-//DEPS com.google.protobuf:protobuf-java:4.31.1
+//DEPS com.google.protobuf:protobuf-java:4.32.1
 //DEPS info.picocli:picocli:4.7.7
 
 
